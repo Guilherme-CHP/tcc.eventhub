32717559fab37c380962cff7c64c730b6a45dbba
Guilherme-CHP <142953359+Guilherme-CHP@users.noreply.github.com>
2024-05-31 18:24:09 -0300

    first commit

--- index.php
+++ index.php
@@ -1,33 +1,3 @@
-<?php
-include ('conexao.php');
-
-if (isset($_POST['email']) || isset($_POST['senha'])) { {
-        $email = $mysqli->real_escape_string($_POST['email']);
-        $senha = $mysqli->real_escape_string($_POST['senha']);
-
-        $sql_code = "SELECT * FROM usuarios WHERE email = '$email' AND senha = '$senha'";
-        $sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL: " . $mysqli->error);
-
-        $quantidade = $sql_query->num_rows;
-
-        if ($quantidade == 1) {
-
-            $usuario = $sql_query->fetch_assoc();
-
-            if (!isset($_SESSION)) {
-                session_start();
-            }
-
-            $_SESSION['id'] = $usuario['id'];
-            $_SESSION['nome'] = $usuario['nome'];
-
-            header("Location: site.php");
-        } else {
-            echo "Falha ao logar! E-mail ou senha incorretos";
-        }
-    }
-};
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -40,7 +10,7 @@ if (isset($_POST['email']) || isset($_POST['senha'])) { {
 
     <div class="login-container">
         <h2>Login no Eventhub</h2>
-        <form action="" method="post">
+        <form action="logSite.php" method="post">
             <div class="form-group">
                 <label for="email">E-mail:</label>
                 <input type="email" id="email" name="email" required>
